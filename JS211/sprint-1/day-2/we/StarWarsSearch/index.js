486be9f34d973e68be1e12b52f78a5ca178662bf
debasistripathy1 <debasistripathy04@gmail.com>
2022-08-12 04:07:34 +0530

    90

--- JS211/sprint-1/day-2/we/StarWarsSearch/index.js
+++ JS211/sprint-1/day-2/we/StarWarsSearch/index.js
@@ -3,7 +3,7 @@
 
 let url = "https://swapi.dev/api/people/?format=json";
 
-
+let Data = JSON.parse(localStorage.getItem("star-war-data"))||[]
 
 
 
@@ -19,14 +19,14 @@ function debounceSearchFunc(DisplayData, delay){
         DisplayData();
     },2000);
 }
-
+let new_data;
 async function DisplayData(){
     let query = document.getElementById("navSearch").value;
     try{
         let res = await fetch(`${url}&search=${query}`);
         let data = await res.json();
 
-        let new_data = data.results;
+        new_data = data.results;
         console.log("data :", new_data);
         // console.log("data : ", data);
 
@@ -36,10 +36,45 @@ async function DisplayData(){
         }
     }
     catch(error){
-
+        console.log(error);
     }
 }
 
-function DisplayData(data){
-    
+function displayMovies(data){
+    let container = document.getElementById("names");
+    container.value = null;
+
+
+    data.forEach((elem)=>{
+        let box = document.createElement("div");
+        box.setAttribute("class", "box");
+
+        let names = document.createElement("p");
+        names.innerText =  elem.name;
+
+
+
+        names.setAttribute("class", "name");
+        names.addEventListener("click", ()=>{
+            submitFunc(elem);
+        });
+        // let mass = document.createElement("p");
+        // mass.innerText =  elem.mass;
+
+        // let skin_color = document.createElement("p");
+        // skin_color.innerText =  elem.skin_color;
+
+        // let total_films = document.createElement("p");
+        // total_films.innerText = elem.films
+
+        // box.append(names, mass, skin_color, total_films);
+        box.append(names);
+        container.append(box);
+    });
+}
+
+function submitFunc(elem){
+    Data.push(elem);
+    localStorage.setItem("star-war-data", JSON.stringify(Data));
+    window.location.href = "./details.html";
 }
