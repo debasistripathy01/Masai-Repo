5687f1aa50074151be3a1de9a1536fe3ab5c0298
debasistripathy1 <debasistripathy04@gmail.com>
2022-08-15 17:13:06 +0530

    95

--- JS211/sprint-1/day-1/you/ImageSlider/index.js
+++ JS211/sprint-1/day-1/you/ImageSlider/index.js
@@ -1,4 +1,57 @@
-// 
+//
+let slideNow = function () {
+  let container = document.getElementById("container");
+  let sliderwidth = container.offsetWidth;
+  let listOfImages = document.getElementById("box");
+  let count = 1;
+  let items = listOfImages.querySelectorAll("#box>div").length;
+  // console.log(items);
+  var  prev = document.getElementById("prev");
+  var next = document.getElementById("next");
 
 
-let container = document.getElementById("container");
+  window.addEventListener("resize", () => {
+    sliderwidth = container.offsetWidth;
+  });
+
+  let prevSlide = () => {
+    if (count > 1) {
+      count -= 2;
+      listOfImages.style.left = "-" + count * sliderwidth + "px";
+      count++;
+    } else if ((count = 1)) {
+      count = +items - 1;
+      listOfImages.style.left = "-" + count * sliderwidth + "px";
+      count++;
+    }
+  };
+
+  let nextSlide = () => {
+    if (count < +items) {
+      listOfImages.style.left = "-" + count * sliderwidth + "px";
+      count++;
+    } else if (count == +items) {
+      listOfImages.style.left = "0px";
+      count = 1;
+    }
+  };
+
+  next.addEventListener("click", () => {
+    nextSlide();
+  });
+
+  prev.addEventListener("click", () => {
+    prevSlide();
+  });
+
+  setInterval(function () {
+    nextSlide();
+  }, 8000);
+
+//   setInterval(function () {
+//     prevSlide();
+//   }, 8000);
+};
+window.onload = function () {
+  slideNow();
+};
