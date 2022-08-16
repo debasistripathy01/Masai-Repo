70436ee93fa14011b588056330a874f4afbdd8c0
debasistripathy1 <debasistripathy04@gmail.com>
2022-08-16 17:03:22 +0530

    99

---
+++ JS211/sprint-3/evaluation/news_app_561529/scripts/sidebar.js
@@ -0,0 +1,20 @@
+
+
+
+function sidebar(){
+    return ` <div>
+    <img src="" alt="">
+  </div>
+  <div>
+    <p>Name : </p>
+  </div>
+  <div>
+    <p>Email : </p>
+  </div>
+  <div>
+    <p>
+      Country
+    </p> `
+}
+
+export default sidebar;
