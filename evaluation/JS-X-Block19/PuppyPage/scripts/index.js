73042f61a0dbc6f04cd8fd908e177db03f74ee20
debasistripathy1 <debasistripathy04@gmail.com>
2022-09-15 17:12:21 +0530

    commit 2

--- evaluation/JS-X-Block19/PuppyPage/scripts/index.js
+++ evaluation/JS-X-Block19/PuppyPage/scripts/index.js
@@ -6,3 +6,51 @@
 
 // - On page load display names of all breeds of dogs in form of cards.
 // - On click of any breed name redirect to `puppy.html` and display all images of that particular breed using this api.
+
+let pupppyArr = [];
+
+
+async function getPuppy(){
+    let url = "https://dog.ceo/api/breeds/list/all";
+
+    try{
+        let res  = await fetch(url);
+        let Data = await res.json();
+
+        let new_data = Data.message;
+        console.log("Data : ", new_data);
+
+        if(new_data!==undefined){
+            pupppyArr = new_data;
+            DisplayData(new_data);
+        }
+
+    }
+    catch(error){
+        console.log(error);
+    };
+}
+
+getPuppy();
+
+
+function DisplayData(new_data){
+
+    let container = document.getElementById("names");
+    container.innerHTML  =null;
+
+    new_data.forEach((el)=>{
+
+        let box = document.createElement("div");
+        box.setAttribute("class", "puppyNames");
+
+        let btn = document.createElement("button");
+        btn.innerText = el;
+
+
+        box.append(btn);
+        container.append(box);
+    });
+}
+
+
