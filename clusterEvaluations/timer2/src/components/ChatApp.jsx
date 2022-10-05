f7af4d1711b9519632aeb338d32018c34ceb2cd4
debasistripathy1 <debasistripathy04@gmail.com>
2022-10-05 20:18:13 +0530

    chatApp

--- clusterEvaluations/timer2/src/components/ChatApp.jsx
+++ clusterEvaluations/timer2/src/components/ChatApp.jsx
@@ -1,38 +1,37 @@
-
-
-import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import createConnection from "../utils/chatutils";
 import { users } from "../utils/chatutils";
 import Contacts from "./contact";
 
-export default function ChatApp(){
+function ChatApp() {
+  const [messages, setMessages] = useState([]);
+  const [subscribedTo, setSubscribedTo] = useState(users[0]);
 
-    const [messages, setMessages] = useState([]);
-    const [subscribed, setSubscribe] = useState(users[0]);
-
-    useEffect((el)=>{
-
-
-        const [listen, unsubscribe] = useState(setSubscribe);
-        listen((messages)=>{
-            setMessages((prevMessage)=>{
-                [...prevMessage, messages ]
-            })
-        });
-        const cleanUp=()=>{
-            unsubscribe();
-            console.log("clean up called");
-
-        }
-        return cleanUp;
-    },[subscribed])
-
-    return (
-        <div className="application">
-            <h1>COntact </h1>
-            <Contacts />
-            
-
-        </div>
-    )
+  // - const {listen, unsubscribe} = creteConnection( currentUser )
+  // - const connection = creteConnection( currentUser )
+  // - listen( callback )
+  // - the callback will be given any new message
+  // - so you can write listen( message => {...your code} )
+  // - you need to ensure you are unsubscribing from user1 to user2 as well
+  // - display all the messages on the UI
+  // - when changing user, messages should be reset
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <Contacts
+        // all users
+        users={users}
+        // active user
+        active={subscribedTo}
+        // onChange
+        onChange={(user) => setSubscribedTo(user)}
+      />
+      <hr />
+      {messages.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </div>
+  );
 }
+
+export default ChatApp;
--- clusterEvaluations/timer2/src/components/contact.jsx
+++ clusterEvaluations/timer2/src/components/contact.jsx
@@ -1,21 +1,18 @@
-import React from "react";
-
-
-export default function Contacts({users, active, onChange}){
-
-
+function Contacts({ users, active, onChange }) {
     return (
-        <div>
-            {
-                users.map((el)=>
-                {
-                    <div style={{background: active===el ? "teal" : null}}
-                    key={el.id}
-                    onClick={()=>onChange(el)}>{el.name}</div>
-                })
-            }
-
-        </div>
-    )
-
-}
+      <div>
+        {users.map((user) => (
+          <div
+            style={{ background: active === user ? "tomato" : null }}
+            key={user.id}
+            onClick={() => onChange(user)}
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default Contacts;
+  
