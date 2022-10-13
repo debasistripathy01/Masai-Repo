579624420956c160c2a7bd640e6939ecbcfc17dc
debasistripathy1 <debasistripathy04@gmail.com>
2022-10-14 03:40:48 +0530

    todo-statusToggleComplete

--- clusterEvaluations/todoapp/src/components/Todo.jsx
+++ clusterEvaluations/todoapp/src/components/Todo.jsx
@@ -29,22 +29,45 @@ const addTodos =async(todo)=>{
   })
 }
 
-let Todo = () => {
+// const toggleTodos = (id, newStatus) =>{
+//   return fetch("http://localhost:5000/tasks",{
+// }
+
+const toggleTodos =async(id, newStatus)=>{
+  return fetch(`http://localhost:5000/tasks/${id}`,{
+    method:"PATCH",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({status:newStatus})
+
+  }).then((res)=>{
+    res.json()
+  })
+}
+
+
+// Adding ToDOs
+const Todo = () => {
 
 const[todos, setTodos] = useState([])
-
+const [loading, setLoading] = useState(false)
   
 useEffect(()=>{
   handleTodos();
 },[]);
 
+
 const handleTodos=()=>{
+  setLoading(true)
   FetchTodo().then((res)=>{
+    setLoading(false)
     setTodos(res);
     // console.log(res);
 
   }).catch((error)=>{
     console.log(error);
+    setLoading(false)
   })
 }
 
@@ -52,18 +75,27 @@ const handleAdd =(text)=>{
   const Item ={
     title: text,
     status: false,
-
-  }
+  };
+  setLoading(false)
   addTodos(Item).then((res)=>{
     handleTodos();
     console.log(res);
   })
 
 }
+const handleToggle = (id, newStatus) =>{
+  setLoading(true);
+  toggleTodos(id, newStatus).then((res)=>{
+    handleTodos()
+  }).catch((error)=>{
+    setLoading(false);
+  })
+}
 
 
   return (
     <div>
+      <div>{loading && "...Loading"}</div>
       <h1>Add To DO</h1>
       <AddTodo handleAdd={handleAdd}/>
       {
@@ -73,6 +105,7 @@ const handleAdd =(text)=>{
           key={item.id}
           status={item.status}
           id={item.id}
+          handleToggle={handleToggle}
           />)
         })
       }
--- clusterEvaluations/todoapp/src/components/TodoList.jsx
+++ clusterEvaluations/todoapp/src/components/TodoList.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const TodoList = ({title, status, id}) => {
+const TodoList = ({title, status, id, handleToggle}) => {
 
     // function styles(){
     //     "backgroundColor"= red,
     //     "marginRight"= "10px"
     // }
   return (
-    <div>
+    <div style={{display:"flex", gap:"1rem", margin:"auto", justifyContent:"space-around"}}>
         <b style={{"backgroundColor":"red", "marginRight":"10px"}}>{title}</b>
         {status?"DONE" : "Not Done"}
+        <button onClick={()=>{handleToggle(id, !status)}}>Status</button>
     </div>
   )
 }
