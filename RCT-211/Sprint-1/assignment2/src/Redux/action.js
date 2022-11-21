043b7da8e5fea637fcbbd6a7c4ba813228d71095
debasistripathy01 <debasistripathy04@gmail.com>
2022-11-21 09:47:44 +0530

    counter

--- RCT-211/Sprint-1/assignment2/src/Redux/action.js
+++ RCT-211/Sprint-1/assignment2/src/Redux/action.js
@@ -1,21 +1,21 @@
 //DO NOT change the function names
-import { store } from "./store";
+// import { store } from "./store";
 import { ADD, REDUCE } from "./actionTypes";
 //function to return the add action object
 
-const { dispatch } = store
+// const { dispatch } = store
 
 
 
 
 
-const handleAddActionObj = () => {
-    dispatch({type:ADD, payload:2})
+const handleAddActionObj = (payload) => {
+    return {type:ADD, payload}
 };
 
 //function to return the reduce action object
-const handleReduceActionObj = () => {
-    dispatch({type:REDUCE, payload:2})
+const handleReduceActionObj = (payload) => {
+    return {type:REDUCE, payload}
 };
 
 export { handleAddActionObj, handleReduceActionObj };
