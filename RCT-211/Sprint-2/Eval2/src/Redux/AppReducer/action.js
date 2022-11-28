115c70fb4655410ec4981e48e81c331a21d82b0f
debasistripathy01 <debasistripathy04@gmail.com>
2022-11-28 19:54:56 +0530

    envfile

--- RCT-211/Sprint-2/Eval2/src/Redux/AppReducer/action.js
+++ RCT-211/Sprint-2/Eval2/src/Redux/AppReducer/action.js
@@ -5,11 +5,11 @@ import * as types from "./actionType";
 
 import axios from "axios";
 
-
+const port = process.env.REACT_APP_JSON_SERVER_PORT;
 const getShoes = (params) =>(dispatch) =>{
     dispatch({type:types.GET_SHOES_DATA_REQUEST})
     return axios
-    .get("http://localhost:8080/shoes",params)
+    .get(`http://localhost:${port}/shoes`,params)
     .then((r) =>{
         dispatch({type:types.GET_SHOES_DATA_SUCCESS,payload:r.data})
     })
