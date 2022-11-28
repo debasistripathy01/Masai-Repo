968fbb4a4c0cd0ac20743fdc5f18aabb05f8841e
debasistripathy01 <debasistripathy04@gmail.com>
2022-11-28 19:15:56 +0530

    eval2

--- RCT-211/Sprint-2/Eval2/src/Components/ReqAuth.jsx
+++ RCT-211/Sprint-2/Eval2/src/Components/ReqAuth.jsx
@@ -2,11 +2,11 @@ import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 //Create the HOC for protected Routes
 const ReqAuth = ({ children }) => {
-    const auth = useSelector((state)=> state.AuthReducer.isAuth)
+    const isAuth = useSelector((state)=> state.AuthReducer.isAuth)
 
     const location = useLocation();
-    if(!auth){
-        return <Navigate to="/login" replace state={{ data: location.pathname}} />
+    if(!isAuth){
+        return <Navigate to="/login" replace state={{ from: location }} />
     }
 
     return children;
