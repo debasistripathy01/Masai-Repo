498dc28af5a003a700864fca3ea074c696599f26
debasistripathy01 <debasistripathy04@gmail.com>
2022-11-28 18:54:02 +0530

    eval

--- RCT-211/Sprint-2/Eval2/src/Components/Navbar.jsx
+++ RCT-211/Sprint-2/Eval2/src/Components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const  isAuth = useSelector((state)=>state.AuthReducer.isAuth);
   return (
-    <div data-testid="navbar">
+    <div data-testid="navbar" style={{padding: "0.5rem 1 rem", display:"flex"}}>
       <div data-testid="navbar-home-link">
         <img
           src="/Adidas_Logo.png"
@@ -13,8 +17,16 @@ const Navbar = () => {
       </div>
 
       <div>
+        {
+          !isAuth && (
+            <div>
+              <button data-testid="navbar-login-button">{<Link to="/login" style={{color: "white", textDecoration: "none"}}>LOGIN</Link>}</button>
+            </div>
+          )
+        }
+
         {/* Link button to /login page, if the user is not authenticated, else don't show it*/}
-        <button data-testid="navbar-login-button">LOGIN</button>
+        
       </div>
     </div>
   );
