fdd00d92345f8c1da6c0ff6115331f1d77771e85
debasistripathy01 <debasistripathy04@gmail.com>
2022-11-28 18:22:36 +0530

    eval2

--- RCT-211/Sprint-2/Eval2/src/Pages/Shoes.jsx
+++ RCT-211/Sprint-2/Eval2/src/Pages/Shoes.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useSearchParams } from "react-router-dom";
 import Filter from "../Components/Filter";
+import ShoeCard from "../Components/ShoeCard";
+import { useEffect } from "react";
+import SingleShoe from "./SingleShoe";
 
 const Shoes = () => {
+
+  const shoes = useSelector((state)=> state.AppReducer.shoes)
+  const [searchParams] = useSearchParams();
+  console.log(shoes)
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(location || shoes.length == 0){
+      let shoesParams = {
+        params: {
+          category: searchParams.getAll("category"),
+
+        }
+      };
+
+    }
+  },[location.search]);
+
+
   return (
-    <div>
+    <div style={{display: "flex"}}>
       <Filter />
       <div>
+        { shoes.length > 0 && shoes.map(((SingleShoe)=>{
+          return (
+            <ShoeCard key={SingleShoe.id} shoeId={SingleShoe.id} shoeData={SingleShoe}/>
+          )
+        }))}
         {/* Map through the shoes list here using ShoeCard Component */}
       </div>
     </div>
