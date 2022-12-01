2e039637ee58929db00c74feaca20921333f8398
debasistripathy01 <debasistripathy04@gmail.com>
2022-12-01 10:21:19 +0530

    useFetch

--- RCT-211/Sprint-3/week-3-custom-hookah/src/App.js
+++ RCT-211/Sprint-3/week-3-custom-hookah/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import './App.css';
 import { useTimeOutComp } from "./Component/TimeOutComp";
+import { useFetch } from "./Component/useFetchCustomHook";
 import { useTimeOut } from "./Hooks/useTimeOut";
 import { Todos } from "./Todos/Todos";
 
@@ -10,7 +11,7 @@ import { Todos } from "./Todos/Todos";
 function App() {
 
 
-
+  const { loading, data, error } = useFetch("https://reqres.in/data");
   // if you are returning object then deStructure it here like { ready } otherwise simply write this thats it
   const ready = useTimeOut(5000)
 
@@ -21,6 +22,9 @@ function App() {
     { ready && <h1>App Component: {ready}</h1>}
     <h2>State: {ready ? "true" : "false"}</h2>
     <Todos />
+    { loading && <p>{loading}</p>}
+    { loading && <p>"{data}"</p>}
+    { loading && <p>{error}</p>}
     </div>
   );
 }
