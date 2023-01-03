2f423afebbd90dc63faa1dad4a5bfd95968bc5fa
debasistripathy01 <debasistripathy04@gmail.com>
2023-01-03 09:41:09 +0530

    update

--- NEM111/sprint-3/day-2/index.js
+++ NEM111/sprint-3/day-2/index.js
@@ -14,17 +14,67 @@ app.get("/",(req, res)=>{
 })
 
 app.get("/heros", async(req, res)=>{
-    const heros = await HeroModel.find()
+    // let query = req.query;
+
+    try{
+        const city = req.query.city
+    const language= req.query.language
+    const active = req.query.isActive;
+    const heros = await HeroModel.find({language: language, active: active})
     res.send(heros)
+    }
+    catch(err){
+        console.log(err);
+        console.log("Error getting query")
+    }
+    
+
+})
+
+// Patching the data in the Server
+
+app.patch("/edithero/:id", async(req, res)=>{
+
+
+    try{
+
+        const ID = req.params.id
+        await HeroModel.findByIdAndUpdate({_id: ID}, payload)
+        const payload = req.body;
+        res.send('updated the Hero Data which id is ${ID}')
+
+    }catch(err){
+        console.log(err);
+        console.log("error while patching");
+    }
 })
 
 
+// Deleteing the data in the Server
+
+app.delete("/delete/:id", async( req, res)=>{
+    try{
+        const ID = req.body.id;
+        await HeroModel.findByIdAndDelete({_id:ID});
+        res.send(`Deleted the Hero data of id ${ID}`)
+    }
+    catch(err){
+        console.log(err, "Error while deleting data");
+    }
+})
+
 app.post("/addhero", async(req, res)=>{
-    const data  = req.body;
-    const hero = new HeroModel(data);
-    await hero.save();
-    console.log(data);
-    res.send("added data")
+    try{
+
+        const data  = req.body;
+        const hero = new HeroModel(data);
+        await hero.save();
+        console.log(data);
+        res.send("added data")
+    }catch(err){
+        console.log(err);
+        console.log("Error while doing post request")
+    }
 })
 
 
