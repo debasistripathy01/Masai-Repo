c91b1237a542124fc0dc560ef428271ff53a4506
debasistripathy01 <debasistripathy04@gmail.com>
2023-02-02 12:30:00 +0530

    mockupdate

--- Mocks/mock1/index.js
+++ Mocks/mock1/index.js
@@ -1,116 +1,145 @@
-// API = https://jsonplaceholder.typicode.com/comments
+// // API = https://jsonplaceholder.typicode.com/comments
 
-let array;
+// let array;
+let limit = 10;
+    let page = 1;
+    let post= 1;
+    
+    const extraData = ()=>{
+        setTimeout(()=>{
+            page++
+            getData()
+        },200)
+    }
+
+    const getData = async ()=>{
+        try{
+          const res =  await fetch(`https://jsonplaceholder.typicode.com/comments?_limit=${limit}$_page=${page}`)
+          const data =  await res.json()
+
+        //   / const getItems =async()=>{
 
 
-const getItems =async()=>{
+
+            //     const url = "https://jsonplaceholder.typicode.com/comments";
+            
+            //     try{
+            
+            //         let res = await fetch(url);
+            //         let data = await res.json();
+                    
+            //         // DisplayData(data.map({el})=>el);
+            //         let array = data.map(({name, email, body})=>(name, email, body))
+            // // console.log(array.length);
+            //     DisplayData(data)
+          data.forEach((el,ind)=>{
+            const show = `
+            <div id="posts">
+                <h2 id="post_id">${post++}</h2>
+                <p id="post_body">Name:-${el.name}</p>
+                <p id="post_body">Body:-${el.body}</p>
+            </div>
+            `
+            post++
+            cont.insertAdjacentHTML("beforeend",show)
+        })
+       
 
 
-
-    const url = "https://jsonplaceholder.typicode.com/comments";
-
-    try{
-
-        let res = await fetch(url);
-        let data = await res.json();
-        
-        // DisplayData(data.map({el})=>el);
-        let array = data.map(({name, email, body})=>(name, email, body))
-// console.log(array.length);
-    DisplayData(data)
-    cont.insertAdjacentHTML("beforeend",show)
     }catch(err){
         console.log(err)
     }
 }
 
-getItems();
+
+// getData()
+//     const getAnother = ()=>{
+//         setTimeout(()=>{
+//             page++
+//             getData()
+//         },200)
+//     }
+// // getItems();
 
 
-const data = document.getElementById("container");
-const DisplayData =(array)=>{
+// // const data = document.getElementById("container");
+// const DisplayData =(array)=>{
     
     
-    // const container = document.getElementById("container");
-    const container = document.createElement("container");
-    array.forEach((ele)=>{
-        let box = document.createElement("div");
+//     // const container = document.getElementById("container");
+//     const container = document.createElement("container");
+//     container.setAttribute("id", "mainDiv")
+//     array.forEach((ele, index)=>{
+
+//             const show = `
+//             <div id="posts">
+//                 <h2 id="post_id">${post++}</h2>
+//                 <p id="post_body">Name:-${el.name}</p>
+//                 <p id="post_body">Body:-${el.body}</p>
+//                 <button onClick= id="post_body">Details</button>
+//             </div>
+//             `
+//             cont.insertAdjacentHTML("beforeend",show)
+//         })
+
+//         let btn = document.createElement("button")
+//         btn.setAttribute("id","clickOnit")
+//         btn.addEventListener("click", ()=>{
+//             const Box = document.createElement("span");
+//             Box.setAttribute("class","Box" )
+//             Box.append(box)
+
+//         })
 
 
-        box.setAttribute("class","item");
-
-        let postId = document.createElement("p");
-        postId.innerText = ele.id
-
-        let name = document.createElement("p");
-        name.innerText = ele.name;
-
-        let comments = document.createElement("p");
-        comments.innerText = ele.body;
-
-        
-
-        let btn = document.createElement("button")
-        btn.setAttribute("id","clickOnit")
-        btn.addEventListener("click", ()=>{
-            const Box = document.createElement("span");
-            Box.setAttribute("class","Box" )
-            Box.append(box)
-
-        })
+//         btn.innerText = "Details"
 
 
-        btn.innerText = "Details"
-
-
-        // let hide = document.createElement("button");
+//         // let hide = document.createElement("button");
 
 
 
-        box.append(postId, name, comments, btn);
-        container.append(box)
-    });
-
-    var count =1;
-    for(var i=0; i < array.length; i++){
-        var item = document.createElement("div");
-        item.setAttribute("id", "scrollDiv")
-        item=container;
-        data.appendChild(item)
-    }
+//         box.append(postId, name, comments, btn);
+//         container.append(box)
+//         data.insertAdjacentElement("beforeend",box)
 
 
     
-}
+// }
 
-let limit = 10;
-    let page = 1;
-    let post= 1;
-    const getData = ()=>{
-        setTimeout(()=>{
-            page++
-            getItems()
-        },200)
-    }
+// let limit = 10;
+//     let page = 1;
+//     let post= 1;
+//     const getData = ()=>{
+//         setTimeout(()=>{
+//             page++
+//             getItems()
+//         },200)
+//     }
 
+//     window.addEventListener("scroll",()=>{
+//         const {scrollHeight,scrollTop,clientHeight} = document.documentElement
+//         if(scrollTop+clientHeight>=scrollHeight){
+//             getData()
+//         }
+//     })
+
+
+// data.addEventListener('scroll', function () {
+//     if (data.scrollTop + data.clientHeight >= data.scrollHeight) {
+//         newlist();
+//     }
+// });
+
+
+const cont = document.getElementById("container");
+    
+    
+    
+    extraData()
     window.addEventListener("scroll",()=>{
         const {scrollHeight,scrollTop,clientHeight} = document.documentElement
         if(scrollTop+clientHeight>=scrollHeight){
-            getData()
+            extraData()
         }
     })
-
-
-
-
-
-
-
-data.addEventListener('scroll', function () {
-    if (data.scrollTop + data.clientHeight >= data.scrollHeight) {
-        newlist();
-    }
-});
-
-
-// posts(array)
