aa42a897543ae479076c9c6bc2e420934187d7d2
debasistripathy01 <debasistripathy04@gmail.com>
2023-02-04 15:55:15 +0530

    mock

--- Mocks/mock2repeat/index.js
+++ Mocks/mock2repeat/index.js
@@ -52,10 +52,14 @@ const showData = (new_data)=>{
         position.innerText=el.position
 
         let btn = document.createElement("button");
+        btn.id="myBtn"
         btn.innerText = "Details";
         btn.addEventListener("click", ()=>{
             submitNamesBtn(el);
+
         });
+
+
 
 
         box.append( img, name,btn);
@@ -66,8 +70,90 @@ const showData = (new_data)=>{
 
 function submitNamesBtn(el){
     localStorage.setItem("namesOfPlayer", JSON.stringify(el))||[];
-
+    modal.style.display = "block";
+    ModalData(playerData)
 }
 
-
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+let playerData = JSON.parse(localStorage.getItem("namesOfPlayer"))||[];
 // document.getElementById("container").innerText = data
+
+
+// Modal HEre
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+  
+  
+  const ModalData=(playerData)=>{
+  
+      let modal_container = document.getElementById("modal_container");
+          modal_container.innerHTML=null
+  
+  
+          let box = document.createElement("div");
+          box.setAttribute("class", "modal_playerNames");
+  
+          let img = document.createElement("img");
+          img.setAttribute("class", "modal_image");
+          img.src= "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" 
+  
+  
+  
+          let name = document.createElement("p");
+          name.setAttribute("class", "modal_name");
+          name.innerText="Name : "+playerData.first_name + playerData.last_name
+  
+  
+          let position = document.createElement("p");
+          position.setAttribute("class", "modal_position");
+          position.innerText="Position : "+playerData.position
+          
+          
+          // Team Details Div 
+          let teamBox = document.createElement("div");
+          teamBox.setAttribute("class", "modal_teamBox");
+          teamBox.innerText="Team Details"
+  
+  
+          // Team Details Data 
+          let {abbreviation, city, conference, division, full_name}=playerData.team
+          let abbr = document.createElement("p");
+          abbr.setAttribute("class", "modal_abbreviation");
+  
+          abbr="Abbr : "+abbreviation
+  
+      let cit = document.createElement("p");
+          cit.setAttribute("class", "modal_city");
+          cit.innerText="city: "+city
+  
+          let confe = document.createElement("p");
+          confe.setAttribute("class", "modal_conference");
+          confe.innerText="Conf : "+
+  
+  
+          // div.setAttribute("class", "modal_division");
+          // div.innerText="Div : "+division
+  
+          // let fullName = document.createElement("p");
+          // fullName.setAttribute("class", "modal_fullName");
+          // fullName.innerText=full_name
+  
+  
+          // teamBox.appendChild(abbreviation, city, conference, division, fullName)
+          teamBox.append(abbreviation, city, conference, division, full_name);
+  
+          box.append( img, name, position);
+          modal_container.append(box, teamBox);
+  
+  }
